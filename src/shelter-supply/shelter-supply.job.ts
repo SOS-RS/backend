2ea07895a7b28088fa629e5d22f0b27ca4538cb0
Gustavo Clemente <gusclms@gmail.com>
2024-05-09 18:58:19 -0300

    Added a cron job to ShelterSupply module to run every 30 minutes to change priority of expired supplies (urgent older than 4 hours)

---
+++ src/shelter-supply/shelter-supply.job.ts
@@ -0,0 +1,63 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { PrismaService } from '../prisma/prisma.service';
+import { addHours } from '@/utils';
+import { priorityExpiryInterval } from './constants';
+import { SupplyPriority } from '../supply/types';
+import { ShelterSupplyService } from './shelter-supply.service';
+
+@Injectable()
+export class ShelterSupplyJob {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly shelterSupplyService: ShelterSupplyService,
+  ) {}
+
+  private readonly logger = new Logger(ShelterSupplyJob.name);
+
+  @Cron(CronExpression.EVERY_30_MINUTES)
+  async handleCron() {
+    this.logger.log(`${ShelterSupplyJob.name} running`);
+
+    const expiryDate = addHours(
+      new Date(Date.now()),
+      -priorityExpiryInterval,
+    ).toString();
+
+    const outatedSupplies = await this.prismaService.shelterSupply.findMany({
+      where: {
+        AND: [
+          { priority: SupplyPriority.Urgent },
+          {
+            OR: [
+              {
+                createdAt: {
+                  lte: expiryDate,
+                },
+              },
+              {
+                updatedAt: {
+                  lte: expiryDate,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      select: {
+        supplyId: true,
+        shelterId: true,
+      },
+    });
+
+    outatedSupplies.forEach(async (s) => {
+      const { shelterId, supplyId } = s;
+      await this.shelterSupplyService.update({
+        data: {
+          priority: SupplyPriority.Needing,
+        },
+        where: { shelterId: shelterId, supplyId: supplyId },
+      });
+    });
+  }
+}
--- src/shelter-supply/shelter-supply.module.ts
+++ src/shelter-supply/shelter-supply.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { ShelterSupplyService } from './shelter-supply.service';
 import { ShelterSupplyController } from './shelter-supply.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ShelterSupplyJob } from './shelter-supply.job';
 
 @Module({
   imports: [PrismaModule],
-  providers: [ShelterSupplyService],
+  providers: [ShelterSupplyService, ShelterSupplyJob],
   controllers: [ShelterSupplyController],
 })
 export class ShelterSupplyModule {}

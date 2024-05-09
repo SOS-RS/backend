2e8c59323e6193b62bc3469a742230e1d2267c2f
Joao Meyer <1994meyer@gmail.com>
2024-05-08 22:55:13 -0300

    Working version

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,26 +2,40 @@ import { configure as serverlessExpress } from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { MigrateDeploy } from '@prisma/migrate/dist/commands/MigrateDeploy';
 
 let cachedServer;
+let shouldMigrate = true;
 
 export const handler = async (event, context) => {
+  // It is very annoying to run prisma migrations programatically...
+  //
+  // Hack adapted from:
+  //  https://github.com/prisma/prisma/issues/4703#issuecomment-1430112147
+  if (shouldMigrate) {
+    await new MigrateDeploy().parse([]);
+    shouldMigrate = false;
+  }
+
+  // Start the server instance
   if (!cachedServer) {
     const app = await NestFactory.create(AppModule);
-    
+
     const config = new DocumentBuilder()
       .setTitle('SOS - Rio Grande do Sul')
       .setDescription('...')
       .setVersion('1.0')
       .build();
+
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
     await app.init();
-    cachedServer = serverlessExpress({
-      app: app.getHttpAdapter().getInstance(),
-    });
+
+    const instance = app.getHttpAdapter().getInstance();
+    cachedServer = serverlessExpress({ app: instance });
   }
 
+  // Serve the request
   return cachedServer(event, context);
 };

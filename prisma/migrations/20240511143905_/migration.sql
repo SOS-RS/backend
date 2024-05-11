e826c672cc3feb7d30e6894855ed6f54d640f776
Danilo Romano <danilo.romano@datagrupo.com.br>
2024-05-11 12:52:47 -0300

    fix: corrigindo tipo do dado e adicionando default value

--- prisma/migrations/20240511143905_/migration.sql
+++ prisma/migrations/20240511143905_/migration.sql
@@ -4,28 +4,35 @@
   Esse script usa um cast do postgres para converter as colunas para o tipo correto ao invés de dropá-las
   Assumo que as timestamp estão sendo salvas em UTC
   Para mudar a migration para setar as datas atuais para uma timezone específica, basta adicionar o statement "AT TIME ZONE" após a conversão
-  alter table "category_supplies" alter column "created_at" type TIMESTAMP using "created_at"::timestamptz  AT TIME ZONE 'America/Sao_Paulo';
+  alter table "category_supplies" alter column "created_at" type TIMESTAMPTZ using "created_at"::timestamptz  AT TIME ZONE 'America/Sao_Paulo';
 */
 
 -- AlterTable
-alter table "category_supplies" alter column "created_at" type TIMESTAMP using "created_at"::timestamptz;
-alter table "category_supplies" alter column "updated_at" type TIMESTAMP using "updated_at"::timestamptz;
+alter table "category_supplies" alter column "created_at" type TIMESTAMPTZ  using "created_at"::timestamptz;
+alter table "category_supplies" alter column "created_at"  set default CURRENT_TIMESTAMP;
+alter table "category_supplies" alter column "updated_at" type TIMESTAMPTZ using "updated_at"::timestamptz;
 -- AlterTable
-alter table "sessions" alter column "created_at" type TIMESTAMP using "created_at"::timestamptz;
-alter table "sessions" alter column "updated_at" type TIMESTAMP using "updated_at"::timestamptz;
+alter table "sessions" alter column "created_at" type TIMESTAMPTZ using "created_at"::timestamptz;
+alter table "sessions" alter column "created_at"  set default CURRENT_TIMESTAMP;
+alter table "sessions" alter column "updated_at" type TIMESTAMPTZ using "updated_at"::timestamptz;
 -- AlterTable
-alter table "shelter_managers" alter column "created_at" type TIMESTAMP using "created_at"::timestamptz;
-alter table "shelter_managers" alter column "updated_at" type TIMESTAMP using "updated_at"::timestamptz;
+alter table "shelter_managers" alter column "created_at" type TIMESTAMPTZ using "created_at"::timestamptz;
+alter table "shelter_managers" alter column "created_at"  set default CURRENT_TIMESTAMP;
+alter table "shelter_managers" alter column "updated_at" type TIMESTAMPTZ using "updated_at"::timestamptz;
 -- AlterTable
-alter table "shelter_supplies" alter column "created_at" type TIMESTAMP using "created_at"::timestamptz;
-alter table "shelter_supplies" alter column "updated_at" type TIMESTAMP using "updated_at"::timestamptz;
+alter table "shelter_supplies" alter column "created_at" type TIMESTAMPTZ using "created_at"::timestamptz;
+alter table "shelter_supplies" alter column "created_at"  set default CURRENT_TIMESTAMP;
+alter table "shelter_supplies" alter column "updated_at" type TIMESTAMPTZ using "updated_at"::timestamptz;
 -- AlterTable
-alter table "shelters" alter column "created_at" type TIMESTAMP using "created_at"::timestamptz;
-alter table "shelters" alter column "updated_at" type TIMESTAMP using "updated_at"::timestamptz;
+alter table "shelters" alter column "created_at" type TIMESTAMPTZ using "created_at"::timestamptz;
+alter table "shelters" alter column "created_at"  set default CURRENT_TIMESTAMP;
+alter table "shelters" alter column "updated_at" type TIMESTAMPTZ using "updated_at"::timestamptz;
 -- AlterTable
-alter table "supplies" alter column "created_at" type TIMESTAMP using "created_at"::timestamptz;
-alter table "supplies" alter column "updated_at" type TIMESTAMP using "updated_at"::timestamptz;
+alter table "supplies" alter column "created_at" type TIMESTAMPTZ using "created_at"::timestamptz;
+alter table "supplies" alter column "created_at"  set default CURRENT_TIMESTAMP;
+alter table "supplies" alter column "updated_at" type TIMESTAMPTZ using "updated_at"::timestamptz;
 -- AlterTable;
-alter table "users" alter column "created_at" type TIMESTAMP using "created_at"::timestamptz;
-alter table "users" alter column "updated_at" type TIMESTAMP using "updated_at"::timestamptz ; 
+alter table "users" alter column "created_at" type TIMESTAMPTZ using "created_at"::timestamptz;
+alter table "users" alter column "created_at"  set default CURRENT_TIMESTAMP;
+alter table "users" alter column "updated_at" type TIMESTAMPTZ using "updated_at"::timestamptz ;
 

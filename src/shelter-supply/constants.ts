84dfc746d3628957257869230a6c667c7fe43590
Gustavo Clemente <gusclms@gmail.com>
2024-05-11 17:07:44 -0300

    Added constants to .env; changed implementation to new date types and use one db query to update needed supplies

--- src/shelter-supply/constants.ts
+++ src/shelter-supply/constants.ts
@@ -1,3 +1,8 @@
-const priorityExpiryInterval = 4; // In hours
+const jobInterval: string =
+  process.env.SHELTER_SUPPLY_PRIORITY_JOB_INTERVAL ?? '0 */30 * * * *';
 
-export { priorityExpiryInterval };
+const priorityExpiryInterval: number = Number.parseInt(
+  process.env.SHELTER_SUPPLY_PRIORITY_EXPIRY_INTERVAL ?? '4',
+);
+
+export { jobInterval, priorityExpiryInterval };
--- src/shelter-supply/shelter-supply-expiration.job.ts
+++ src/shelter-supply/shelter-supply-expiration.job.ts
@@ -1,8 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import { PrismaService } from '../prisma/prisma.service';
 import { addHours } from '@/utils';
-import { priorityExpiryInterval } from './constants';
+import { priorityExpiryInterval, jobInterval } from './constants';
 import { SupplyPriority } from '../supply/types';
 import { ShelterSupplyService } from './shelter-supply.service';
 
@@ -15,32 +15,21 @@ export class ShelterSupplyExpirationJob {
 
   private readonly logger = new Logger(ShelterSupplyExpirationJob.name);
 
-  @Cron(CronExpression.EVERY_30_MINUTES)
+  @Cron(jobInterval)
   async handleCron() {
     this.logger.log(`${ShelterSupplyExpirationJob.name} running`);
 
     const expiryDate = addHours(new Date(Date.now()), -priorityExpiryInterval);
-    const supplies = await this.prismaService.shelterSupply.findMany({
-      select: {
-        supplyId: true,
-        shelterId: true,
-        createdAt: true,
+    await this.prismaService.shelterSupply.updateMany({
+      data: {
+        priority: SupplyPriority.Needing,
       },
       where: {
         priority: SupplyPriority.Urgent,
+        createdAt: {
+          lte: expiryDate,
+        },
       },
-    });
-
-    supplies.forEach(async (s) => {
-      const { shelterId, supplyId, createdAt } = s;
-      if (new Date(createdAt) <= expiryDate) {
-        await this.shelterSupplyService.update({
-          data: {
-            priority: SupplyPriority.Needing,
-          },
-          where: { shelterId: shelterId, supplyId: supplyId },
-        });
-      }
     });
   }
 }

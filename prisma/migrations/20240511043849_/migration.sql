7530c3998cd853b1469bc34662d18a0c946af1cc
Henrique <henriquecfreitas@hotmail.com.br>
2024-05-11 01:45:03 -0300

    Adding city and postcode to shelter schema

---
+++ prisma/migrations/20240511043849_/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "shelters"
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "street" TEXT,
+ADD COLUMN     "street_number" TEXT,
+ADD COLUMN     "neighbourhood" TEXT,
+ADD COLUMN     "state_district" TEXT,
+ADD COLUMN     "zip_code" TEXT;

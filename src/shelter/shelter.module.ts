b063d5edb7d99894ccda950e6d769171aba79b9d
Henrique <henriquecfreitas@hotmail.com.br>
2024-05-11 03:01:45 -0300

    Implementing geodata fetch cron

--- src/shelter/shelter.module.ts
+++ src/shelter/shelter.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from "@nestjs/schedule"
 
+import { PopulateShelterCity } from './populateShelterCity.cron';
 import { ShelterService } from './shelter.service';
 import { ShelterController } from './shelter.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [ShelterService],
+  imports: [
+    PrismaModule,
+    ScheduleModule.forRoot(),
+  ],
+  providers: [
+    ShelterService,
+    PopulateShelterCity,
+  ],
   controllers: [ShelterController],
 })
 export class ShelterModule {}

934652ba485eed2e4ad8061baaf3599f977bb6c7
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-05-12 21:29:08 -0300

    feat: Importing function responsible for centralizing all operations with swagger, and using dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   HttpException,
+  HttpStatus,
   Logger,
   Param,
   Post,
@@ -15,6 +16,9 @@ import { StaffGuard } from '@/guards/staff.guard';
 import { UserGuard } from '@/guards/user.guard';
 import { ServerResponse } from '../utils';
 import { UsersService } from './users.service';
+import { MakeSwagger } from 'src/swagger/swagger.config';
+import { CreateUserDto } from 'src/dto/users/create-user.dto';
+import { UpdateUserDto } from 'src/dto/users/update-user.dto';
 
 @ApiTags('Usuários')
 @Controller('users')
@@ -24,8 +28,18 @@ export class UsersController {
   constructor(private readonly userServices: UsersService) {}
 
   @Post('')
+  @MakeSwagger({
+    operation: {
+      description: 'Create new user',
+      deprecated: false,
+    },
+    responses: [
+      { status: HttpStatus.OK, description: 'Successfully created user' },
+      { status: HttpStatus.BAD_REQUEST, description: 'Failed to create user' },
+    ],
+  })
   @UseGuards(StaffGuard)
-  async store(@Body() body) {
+  async store(@Body() body: CreateUserDto) {
     try {
       await this.userServices.store(body);
       return new ServerResponse(201, 'Successfully created user');
@@ -36,8 +50,18 @@ export class UsersController {
   }
 
   @Put(':id')
+  @MakeSwagger({
+    operation: {
+      description: 'Update user',
+      deprecated: false,
+    },
+    responses: [
+      { status: HttpStatus.OK, description: 'Successfully update user' },
+      { status: HttpStatus.BAD_REQUEST, description: 'Failed to update user' },
+    ],
+  })
   @UseGuards(StaffGuard)
-  async update(@Body() body, @Param('id') id: string) {
+  async update(@Body() body: UpdateUserDto, @Param('id') id: string) {
     try {
       await this.userServices.update(id, body);
       return new ServerResponse(201, 'Successfully updated user');
@@ -48,8 +72,18 @@ export class UsersController {
   }
 
   @Put('')
+  @MakeSwagger({
+    operation: {
+      description: 'Update user',
+      deprecated: false,
+    },
+    responses: [
+      { status: HttpStatus.OK, description: 'Successfully update user' },
+      { status: HttpStatus.BAD_REQUEST, description: 'Failed to update user' },
+    ],
+  })
   @UseGuards(UserGuard)
-  async selfUpdate(@Body() body, @Req() req) {
+  async selfUpdate(@Body() body: UpdateUserDto, @Req() req) {
     try {
       const { userId } = req.user;
       await this.userServices.update(userId, body);

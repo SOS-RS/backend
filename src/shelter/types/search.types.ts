6f8b860845ddb082baaa5281091e13c1641c2342
Anderson Rocha <59173445+AndersonCRocha@users.noreply.github.com>
2024-05-13 20:14:20 -0300

    Criação do endpoint para busca de cidades dos abrigos (#82)
    Co-authored-by: Pedro Perrone <psperrone@hotmail.com>

--- src/shelter/types/search.types.ts
+++ src/shelter/types/search.types.ts
@@ -1,28 +1,43 @@
 import { Shelter, ShelterSupply, Supply } from '@prisma/client';
+import { z } from 'zod';
 import { SupplyPriority } from '../../supply/types';
 
-export type ShelterAvailabilityStatus = 'available' | 'unavailable' | 'waiting';
+const ShelterStatusSchema = z.enum(['available', 'unavailable', 'waiting']);
 
-export interface IFilterFormProps {
-  search: string;
-  priority: SupplyPriority | null;
-  supplyCategoryIds: string[];
-  supplyIds: string[];
-  shelterStatus: ShelterAvailabilityStatus[];
-  tags: ShelterTagInfo | null;
-}
+export type ShelterStatus = z.infer<typeof ShelterStatusSchema>;
+
+const ShelterTagTypeSchema = z.enum([
+  'NeedVolunteers',
+  'NeedDonations',
+  'RemainingSupplies',
+]);
+
+const ShelterTagInfoSchema = z.record(
+  ShelterTagTypeSchema,
+  z.number().optional(),
+);
+
+export type ShelterTagType = z.infer<typeof ShelterTagTypeSchema>;
+
+export type ShelterTagInfo = z.infer<typeof ShelterTagInfoSchema>;
+
+export const ShelterSearchPropsSchema = z.object({
+  search: z.string().optional(),
+  priority: z.preprocess(
+    (value) => Number(value) || undefined,
+    z.nativeEnum(SupplyPriority).optional(),
+  ),
+  supplyCategoryIds: z.array(z.string()).optional(),
+  supplyIds: z.array(z.string()).optional(),
+  shelterStatus: z.array(ShelterStatusSchema).optional(),
+  tags: ShelterTagInfoSchema.nullable().optional(),
+  cities: z.array(z.string()).optional(),
+});
+
+export type ShelterSearchProps = z.infer<typeof ShelterSearchPropsSchema>;
 
 type AllowedShelterFields = Omit<Shelter, 'contact'>;
 
 export type SearchShelterTagResponse = AllowedShelterFields & {
   shelterSupplies: (ShelterSupply & { supply: Supply })[];
-};
-
-export type ShelterTagType =
-  | 'NeedVolunteers'
-  | 'NeedDonations'
-  | 'RemainingSupplies';
-
-export type ShelterTagInfo = {
-  [key in ShelterTagType]?: number;
 };

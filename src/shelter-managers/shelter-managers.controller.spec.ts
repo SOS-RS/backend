97327908e72fb036a3a42e5c563a2cdcf7704986
Giovanni Bassi <334958+giggio@users.noreply.github.com>
2024-05-14 19:43:22 -0300

    Fix tests (#78)

--- src/shelter-managers/shelter-managers.controller.spec.ts
+++ src/shelter-managers/shelter-managers.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShelterManagersController } from './shelter-managers.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ShelterManagersService } from './shelter-managers.service';
 
 describe('ShelterManagersController', () => {
   let controller: ShelterManagersController;
@@ -7,7 +9,14 @@ describe('ShelterManagersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ShelterManagersController],
-    }).compile();
+      providers: [ShelterManagersService],
+    })
+      .useMocker((token) => {
+        if (token === PrismaService) {
+          return {};
+        }
+      })
+      .compile();
 
     controller = module.get<ShelterManagersController>(
       ShelterManagersController,
--- src/shelter-managers/shelter-managers.service.spec.ts
+++ src/shelter-managers/shelter-managers.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShelterManagersService } from './shelter-managers.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('ShelterManagersService', () => {
   let service: ShelterManagersService;
@@ -7,7 +8,13 @@ describe('ShelterManagersService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ShelterManagersService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === PrismaService) {
+          return {};
+        }
+      })
+      .compile();
 
     service = module.get<ShelterManagersService>(ShelterManagersService);
   });

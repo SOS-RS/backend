a88475bc9ecd566479a93f9d13a53532e1b4441d
Giovanni Bassi <334958+giggio@users.noreply.github.com>
2024-05-14 19:43:22 -0300

    Fix tests (#78)

--- src/shelter/shelter.service.spec.ts
+++ src/shelter/shelter.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShelterService } from './shelter.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('ShelterService', () => {
   let service: ShelterService;
@@ -7,7 +8,17 @@ describe('ShelterService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ShelterService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === PrismaService) {
+          return {
+            supplyCategory: {
+              findMany: jest.fn().mockResolvedValue([]),
+            },
+          };
+        }
+      })
+      .compile();
 
     service = module.get<ShelterService>(ShelterService);
   });

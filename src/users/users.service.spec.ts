d814540027e4b0b8346709ad5d120f4e85218ac8
Matheus Dias <matheus.vex@gmail.com>
2024-05-15 19:00:03 -0300

    test: unit tests for user service class

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../../src/prisma/prisma.service';
+import { UpdateUserSchema } from './types';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let userService: UsersService;
+
+  const mockPrismaService = {
+    user: {
+      create: jest.fn(),
+      update: jest.fn()
+    }
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+      providers: [
+        UsersService,
+        PrismaService,
+      ],
+    })
+    .overrideProvider(PrismaService)
+    .useValue(mockPrismaService)
+    .compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
+
+  describe('store function', () => {
+    const userPayload = {
+      name: 'matheus', lastName: 'silva', phone:'44999998311'
+    }
+
+    it(`deve chamar a funcao store 1 vez`, async () => {
+      userService.store(userPayload)
+      expect(mockPrismaService.user.create).toHaveBeenCalledTimes(1)
+    });
+
+    it(`deve chamar a funcao store com os parametros`, async () => {
+      userService.store(userPayload)
+      const data = {
+        ...userPayload,
+        password: userPayload.phone,
+        login: userPayload.phone,
+        createdAt: new Date().toISOString(),
+      }
+      expect(mockPrismaService.user.create).toHaveBeenCalledWith({ data })
+    });
+  });
+
+  describe('update function', () => {
+      const id = 'user_id_test';
+      const body = {
+        name: 'Matheus', 
+        lastName: 'Silva', 
+        phone:'44999998311',
+      };
+
+    it(`deve chamar a funcao store 1 vez`, async () => {
+      userService.update(id, body)
+      expect(mockPrismaService.user.update).toHaveBeenCalledTimes(1)
+    });
+  })
 });

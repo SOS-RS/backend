bc7d6757f43b1f15b56c0d115b992592c72aa0b0
Vinícius <viniciusmvn@pm.me>
2024-05-19 23:11:34 -0300

    test: cover getMany and create for partners entity

--- src/partners/partners.service.spec.ts
+++ src/partners/partners.service.spec.ts
@@ -1,21 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-
 import { PartnersService } from './partners.service';
 import { PrismaService } from '../prisma/prisma.service';
+
+jest.mock('../prisma/prisma.service'); // Mock PrismaService
 
 describe('PartnersService', () => {
   let service: PartnersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PartnersService],
+      providers: [
+        PartnersService,
+        { provide: PrismaService, useClass: PrismaService }, // Provide PrismaService
+      ],
     })
-      .useMocker((token) => {
-        if (token === PrismaService) {
-          return {
-            supplyCategory: { findMany: jest.fn().mockResolvedValue(0) },
-          };
-        }
+      .overrideProvider(PrismaService)
+      .useValue({
+        // Use value for PrismaService
+        partners: {
+          findMany: jest.fn().mockResolvedValue([]), // Mock findMany to return an empty array
+          create: jest.fn().mockResolvedValue({}), // Mock create to return an empty object
+        },
       })
       .compile();
 
@@ -23,6 +28,19 @@ describe('PartnersService', () => {
   });
 
   it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return all partners', async () => {
+    const partners = await service.index();
+    expect(partners).toEqual([]); // Adjusted to check against an empty array
+  });
+
+  it('should create a partner', async () => {
+    await service.store({
+      name: 'Partner 1',
+      link: 'https://partner1.com',
+    });
     expect(service).toBeDefined();
   });
 });

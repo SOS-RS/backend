aef4cf0017fbd746459295b6e3f8eff884143410
Matheus Dias <matheus.vex@gmail.com>
2024-05-20 17:29:30 -0300

    test: refactor controller unit tests for integration tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,119 +1,126 @@
+/* eslint-disable jest/no-conditional-expect */
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ServerResponse } from '../utils';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
-  let userService: UsersService;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [
-        {
-          provide: UsersService,
-          useValue: {
-            store: jest.fn(),
-            update: jest.fn(),
-          },
-        },
-      ],
+      providers: [UsersService, PrismaService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
-    userService = module.get<UsersService>(UsersService);
+    service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-    expect(userService).toBeDefined();
-  });
-
-  describe('store function', () => {
-    const body = {
-      name: 'Matheus',
-      lastName: 'Silva',
-      phone: '44999998311',
-    };
-
-    it('checks if the function has been called 1 time', async () => {
-      await controller.store(body);
-      expect(userService.store).toHaveBeenCalledTimes(1);
-    });
-
-    it('checks the return of the function', async () => {
-      const expectedResult = {
-        message: 'Successfully created user',
-        respData: undefined,
-        statusCode: 201,
+  describe('store', () => {
+    it('should create a new user', async () => {
+      const userData = {
+        name: 'Test',
+        lastName: 'User',
+        phone: '123456789',
       };
-      const result = await controller.store(body);
-      expect(expectedResult).toEqual(result);
+      const response = new ServerResponse(201, 'Successfully created user');
+
+      jest.spyOn(service, 'store').mockResolvedValueOnce(undefined);
+
+      expect(await controller.store(userData)).toEqual(response);
     });
 
-    it('checks if the function fails', async () => {
-      jest.spyOn(userService, 'store').mockRejectedValueOnce(new Error());
-      expect(controller.store(body)).rejects.toThrow();
+    it('should throw error if creation fails', async () => {
+      const body = {
+        name: 'Updated',
+        lastName: 'User',
+        phone: '987654321',
+      };
+      jest
+        .spyOn(service, 'store')
+        .mockRejectedValueOnce(new Error('Store failed'));
+      try {
+        await controller.store(body);
+        throw new Error('Expected update function to throw an error');
+      } catch (error: any) {
+        expect(error.response).toEqual('Error');
+        expect(error.status).toEqual(400);
+      }
     });
   });
 
-  describe('update function', () => {
-    const body = {
-      name: 'Matheus',
-      lastName: 'Silva',
-      phone: '44999998311',
-    };
-    const id = 'test_user_id';
-
-    it('checks if the function has been called 1 time', async () => {
-      await controller.update(body, id);
-      expect(userService.update).toHaveBeenCalledTimes(1);
-    });
-
-    it('checks the return of the function', async () => {
-      const expectedResult = {
-        message: 'Successfully updated user',
-        respData: undefined,
-        statusCode: 201,
+  describe('update', () => {
+    it('should update an existing user', async () => {
+      const userId = '1';
+      const userData = {
+        name: 'Updated',
+        lastName: 'User',
+        phone: '987654321',
       };
-      const result = await controller.update(body, id);
-      expect(expectedResult).toEqual(result);
+      const response = new ServerResponse(201, 'Successfully updated user');
+
+      jest.spyOn(service, 'update').mockResolvedValueOnce(undefined);
+
+      expect(await controller.update(userData, userId)).toEqual(response);
     });
 
-    it('checks if the function fails', async () => {
-      jest.spyOn(userService, 'update').mockRejectedValueOnce(new Error());
-      expect(controller.update(body, id)).rejects.toThrow();
+    it('should throw error if update fails', async () => {
+      const id = '1';
+      const userData = {
+        name: 'Updated',
+        lastName: 'User',
+        phone: '987654321',
+      };
+      jest
+        .spyOn(service, 'update')
+        .mockRejectedValueOnce(new Error('Update failed'));
+      try {
+        await controller.update(userData, id);
+        throw new Error('Expected update function to throw an error');
+      } catch (error: any) {
+        expect(error.response).toEqual('Error');
+        expect(error.status).toEqual(400);
+      }
     });
   });
 
-  describe('selfUpdate function', () => {
-    const body = {
-      name: 'Matheus',
-      lastName: 'Silva',
-      phone: '44999998311',
-    };
-    const req = {
-      user: 'test_user_id',
-    };
-
-    it('checks if the function has been called 1 time', async () => {
-      await controller.selfUpdate(body, req);
-      expect(userService.update).toHaveBeenCalledTimes(1);
-    });
-
-    it('checks the return of the function', async () => {
-      const expectedResult = {
-        message: 'Successfully updated',
-        respData: undefined,
-        statusCode: 201,
+  describe('selfUpdate', () => {
+    it('should update the own user', async () => {
+      const userId = '1';
+      const userData = {
+        name: 'Updated',
+        lastName: 'User',
+        phone: '987654321',
       };
-      const result = await controller.selfUpdate(body, req);
-      expect(expectedResult).toEqual(result);
+      const req = { user: { userId } };
+      const response = new ServerResponse(201, 'Successfully updated');
+
+      jest.spyOn(service, 'update').mockResolvedValueOnce(undefined);
+
+      expect(await controller.selfUpdate(userData, req)).toEqual(response);
     });
 
-    it('checks if the function fails', async () => {
-      jest.spyOn(userService, 'update').mockRejectedValueOnce(new Error());
-      expect(controller.selfUpdate(body, req)).rejects.toThrow();
+    it('should throw error if update fails', async () => {
+      const userId = '1';
+      const userData = {
+        name: 'Updated',
+        lastName: 'User',
+        phone: '987654321',
+      };
+      const req = { user: { userId } };
+      jest
+        .spyOn(service, 'update')
+        .mockRejectedValueOnce(new Error('Update failed'));
+      try {
+        await controller.selfUpdate(userData, req);
+        throw new Error('Expected update function to throw an error');
+      } catch (error: any) {
+        expect(error.response).toEqual('Error');
+        expect(error.status).toEqual(400);
+      }
     });
   });
 });

ada487b5e6771f1053dc46d695e3389af01622fb
Marcos Silva <marcos.201314@hotmail.com>
2024-05-21 22:47:24 -0300

    refact: changing the logic of the query implementations

--- src/modules/dashboard/dashboard.service.ts
+++ src/modules/dashboard/dashboard.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ShelterSearchPropsSchema } from 'src/shelter/types/search.types';
 import { SearchSchema } from 'src/types';
-import { ShelterSearch } from 'src/shelter/ShelterSearch';
+import { parseTagResponse, ShelterSearch } from 'src/shelter/ShelterSearch';
+import { DefaultArgs } from '@prisma/client/runtime/library';
+import { Prisma } from '@prisma/client';
+import { SupplyPriority } from 'src/supply/types';
 
 @Injectable()
 export class DashboardService {
@@ -17,14 +20,16 @@ export class DashboardService {
       perPage,
       search: searchQuery,
     } = SearchSchema.parse(query);
-
     const queryData = ShelterSearchPropsSchema.parse(qs.parse(searchQuery));
-    const { query: where } = new ShelterSearch(this.prismaService, queryData);
+    const { getQuery } = new ShelterSearch(this.prismaService, queryData);
+    const where = await getQuery();
+
+    const count = await this.prismaService.shelter.count({ where });
 
     const take = perPage;
     const skip = perPage * (page - 1);
 
-    const whereData = {
+    const whereData: Prisma.ShelterFindManyArgs<DefaultArgs> = {
       take,
       skip,
       orderBy: { [orderBy]: order },
@@ -33,35 +38,101 @@ export class DashboardService {
 
     const allShelters = await this.prismaService.shelter.findMany({
       ...whereData,
-      include: { shelterSupplies: true },
+      select: {
+        id: true,
+        name: true,
+        pix: true,
+        address: true,
+        street: true,
+        neighbourhood: true,
+        city: true,
+        streetNumber: true,
+        zipCode: true,
+        capacity: true,
+        petFriendly: true,
+        shelteredPeople: true,
+        prioritySum: true,
+        verified: true,
+        latitude: true,
+        longitude: true,
+        actived: true,
+        category: true,
+        createdAt: true,
+        updatedAt: true,
+        shelterSupplies: {
+          where: {
+            priority: {
+              notIn: [SupplyPriority.UnderControl],
+            },
+          },
+          orderBy: {
+            updatedAt: 'desc',
+          },
+          include: {
+            supply: true,
+          },
+        },
+      },
     });
 
-    const allPeopleSheltered = allShelters.reduce((accumulator, current) => {
-      return accumulator + (current.shelteredPeople ?? 0);
-    }, 0);
-
-    let shelterAvailable = 0;
-    let shelterFull = 0;
-    let shelterWithoutInformation = 0;
-
-    allShelters.forEach((shelter) => {
-      if (shelter.shelteredPeople !== null && shelter.capacity !== null) {
-        if (shelter.shelteredPeople < shelter.capacity) {
-          shelterAvailable++;
-        } else if (shelter.shelteredPeople >= shelter.capacity) {
-          shelterFull++;
+    const allCategories = await this.prismaService.supplyCategory.findMany({
+      select: {
+        id: true,
+        name: true,
+        supplies: {
+          select: {
+            id: true,
+            shelterSupplies: {
+              select: {
+                shelter: {
+                  select: {
+                    id: true,
+                    name: true
+                  }
+                }
+              }
+            }
+          }
         }
-      } else {
-        shelterWithoutInformation++;
-      }
+      },
     });
+
+    const categoriesWithDetailsPromises = allCategories.map(async (category) => {
+      const supplies = category.supplies.flatMap(supply => supply.shelterSupplies);
+      const sheltersWithSupplies = supplies.map(supply => supply.shelter);
+    
+      const sheltersRequesting = await this.prismaService.shelter.findMany({
+        where: {
+          shelterSupplies: {
+            some: {
+              supply: {
+                supplyCategory: {
+                  id: category.id
+                }
+              }
+            }
+          }
+        },
+        select: {
+          id: true,
+          name: true
+        }
+      });
+    
+      return {
+        id: category.id,
+        name: category.name,
+        sheltersWithSupplies: sheltersWithSupplies,
+        sheltersRequesting: sheltersRequesting
+      };
+    });
+
+    const categoriesWithDetails = await Promise.all(categoriesWithDetailsPromises);
+
 
     return {
       allShelters: allShelters.length,
-      allPeopleSheltered: allPeopleSheltered,
-      shelterAvailable: shelterAvailable,
-      shelterFull: shelterFull,
-      shelterWithoutInformation: shelterWithoutInformation,
+      categories: categoriesWithDetails
     };
   }
 }

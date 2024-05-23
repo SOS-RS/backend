8480624954bf02b9e6b7f2424b0c808296151b2e
José Fagundes <fagundesjg@outlook.com>
2024-05-23 18:51:36 -0300

    refact: removed hmac from get all shelters

--- src/shelter/shelter.controller.ts
+++ src/shelter/shelter.controller.ts
@@ -17,7 +17,6 @@ import { ServerResponse } from '../utils';
 import { StaffGuard } from '@/guards/staff.guard';
 import { ApplyUser } from '@/guards/apply-user.guard';
 import { UserDecorator } from '@/decorators/UserDecorator/user.decorator';
-import { Hmac } from '@/decorators/Hmac';
 
 @ApiTags('Abrigos')
 @Controller('shelters')
@@ -26,7 +25,6 @@ export class ShelterController {
 
   constructor(private readonly shelterService: ShelterService) {}
 
-  @Hmac()
   @Get('')
   async index(@Query() query) {
     try {

ae57bfae5107974162e0b1f8ee3b7054fb35ddcb
Juliano <cont.juliano@outlook.com>
2024-05-24 11:54:54 -0300

    test: added user service test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,25 +1,50 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { Test } from '@nestjs/testing';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    })
-      .useMocker((token) => {
-        if (token === PrismaService) {
-          return {};
-        }
-      })
-      .compile();
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        { provide: PrismaService, useClass: PrismaService },
+      ],
+    }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
+    await prismaService.user.deleteMany({});
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Should create new user', async () => {
+    const data = {
+      name: 'name',
+      lastName: 'lastName',
+      phone: 'phone',
+      password: 'password',
+      login: 'login',
+      createdAt: new Date().toISOString(),
+    };
+    await service.store(data);
+    const [user] = await prismaService.user.findMany();
+    expect(user.name).toBe(data.name);
+    expect(user.lastName).toBe(data.lastName);
+    expect(user.accessLevel).toBe('User');
+  });
+
+  it('Not should create new user if login already exists', async () => {
+    const data = {
+      name: 'name',
+      lastName: 'lastName',
+      phone: 'phone',
+      password: 'password',
+      login: 'login',
+      createdAt: new Date().toISOString(),
+    };
+    await service.store(data);
+    await expect(service.store(data)).rejects.toThrow();
   });
 });

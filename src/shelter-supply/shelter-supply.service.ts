6ac356b35eeb0d69ab8aaa8e9cbf71e0234e948e
José Fagundes <fagundesjg@outlook.com>
2024-05-30 11:41:35 -0300

    refact: removed update many shelter supplies

--- src/shelter-supply/shelter-supply.service.ts
+++ src/shelter-supply/shelter-supply.service.ts
@@ -2,11 +2,7 @@ import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from '../prisma/prisma.service';
-import {
-  CreateShelterSupplySchema,
-  UpdateManyShelterSupplySchema,
-  UpdateShelterSupplySchema,
-} from './types';
+import { CreateShelterSupplySchema, UpdateShelterSupplySchema } from './types';
 import { SupplyPriority } from '../supply/types';
 
 @Injectable()
@@ -77,50 +73,6 @@ export class ShelterSupplyService {
         updatedAt: new Date().toISOString(),
       },
     });
-  }
-
-  async updateMany(body: z.infer<typeof UpdateManyShelterSupplySchema>) {
-    const { ids, shelterId } = UpdateManyShelterSupplySchema.parse(body);
-
-    const supplies = await this.prismaService.shelterSupply.findMany({
-      where: {
-        shelterId,
-        supplyId: {
-          in: ids,
-        },
-      },
-    });
-
-    const prioritySum = supplies.reduce(
-      (prev, current) => prev + current.priority,
-      0,
-    );
-
-    await this.prismaService.$transaction([
-      this.prismaService.shelter.update({
-        where: {
-          id: shelterId,
-        },
-        data: {
-          prioritySum: {
-            decrement: prioritySum,
-          },
-          updatedAt: new Date().toISOString(),
-        },
-      }),
-      this.prismaService.shelterSupply.updateMany({
-        where: {
-          shelterId,
-          supplyId: {
-            in: ids,
-          },
-        },
-        data: {
-          priority: SupplyPriority.UnderControl,
-          updatedAt: new Date().toISOString(),
-        },
-      }),
-    ]);
   }
 
   async index(shelterId: string) {
